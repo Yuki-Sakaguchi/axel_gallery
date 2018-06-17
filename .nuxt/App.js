50418f9d797f45101a90bb27d62e39f9a5db2d61
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-06-17 23:56:54 +0900

    [modify] firebaseのDBとファイルストレージを導入

--- .nuxt/App.js
+++ .nuxt/App.js
@@ -13,7 +13,7 @@ let layouts = {
 let resolvedLayouts = {}
 
 export default {
-  head: {"title":"axel_gallery","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Nuxt.js project"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
+  head: {"title":"axel_gallery","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Nuxt.js project"}],"script":[{"src":"https:\u002F\u002Fwww.gstatic.com\u002Ffirebasejs\u002F5.0.4\u002Ffirebase.js"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[]},
   render(h, props) {
     const loadingEl = h('nuxt-loading', { ref: 'loading' })
     const layoutEl = h(this.layout || 'nuxt')
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,69 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
       <h1 class="title">axel_gallery</h1>
       <div class="links">
         <nuxt-link to="/about/" class="button--green">about</nuxt-link>
       </div>
+      <div id="image-area" class="image-area"></div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  transition: {
+    enter: () => {
+      initImage();
+    }
   }
+}
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDL74v9w6sB6XO-2RCTQaJUsojTkqtTZqE",
+  authDomain: "axel-gallery.firebaseapp.com",
+  databaseURL: "https://axel-gallery.firebaseio.com",
+  projectId: "axel-gallery",
+  storageBucket: "axel-gallery.appspot.com",
+  messagingSenderId: "586204948365"
+};
+firebase.initializeApp(config);
+
+/** 
+ * 画像の初期化
+ */
+function initImage() {
+  // 画像の名前リスト
+  var imgList = [];
+
+  // firebaseのストレージにアクセス
+  var storage = firebase.storage();
+  var storageRef = storage.ref();
+  var spaceRef = storageRef.child('images');
+
+  // DBからファイル名を取得
+  var db = firebase.firestore();
+  db.collection("images").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      imgList.push(doc.data().file_name);
+    });
+  }).then(() => {
+    // ファイル名を元に画像を取得し、画面に追加
+    var elImgArea = document.getElementById('image-area');
+
+    imgList.forEach((e, i) => {
+      var img = spaceRef.child(e);
+      img.getDownloadURL().then((url) => {
+        var elBlock = document.createElement('div');
+        elBlock.className = 'image-box';
+        elBlock.style.backgroundImage = 'url(' + url + ')';
+        elImgArea.appendChild(elBlock);
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+  })
 }
 </script>
 
@@ -48,5 +95,25 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.image-area {
+  margin: 20px;
+}
+
+.image-box {
+  position: relative;
+  width: 50%;
+  display: inline-block;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  vertical-align: top;
+
+  &:before {
+    content: '';
+    display: block;
+    padding-top: (9 / 16) * 100%
+  }
 }
 </style>
